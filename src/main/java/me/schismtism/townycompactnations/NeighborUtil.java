540740b5c86841fcfa50c0d8cd50d7b29c37c19a
Fruitloopins <49851457+Fruitloopins@users.noreply.github.com>
2023-12-08 09:12:26 +1100

    Refactor

--- src/main/java/me/schismtism/townycompactnations/NeighborUtil.java
+++ src/main/java/me/schismtism/townycompactnations/NeighborUtil.java
@@ -1,30 +1,47 @@
 package me.schismtism.townycompactnations;
 
+import com.palmergames.bukkit.towny.TownyAPI;
+import com.palmergames.bukkit.towny.object.Town;
 import com.palmergames.bukkit.towny.object.TownBlock;
+import com.palmergames.bukkit.towny.object.WorldCoord;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class NeighborUtil {
 
-    //Optimize this shit later, gathers every single chunk not just border chunks. It's ok, I still love you ChatGPT.
-    public static Collection<TownBlock> getNeighbors(Collection<TownBlock> originalPositions) {
+    public static Collection<TownBlock> getNeighboringTownBlocksWithOtherTown(Collection<TownBlock> townBlocks) {
         Collection<TownBlock> result = new ArrayList<>();
 
-        for (TownBlock position : originalPositions) {
-            int x = position.getX();
-            int z = position.getZ();
-            result.add(position);
+        for (TownBlock townBlock : townBlocks) {
+            WorldCoord centralWorldCoord = townBlock.getWorldCoord();
+            List<WorldCoord> neighboringWorldCoords = getNeighboringWorldCoords(centralWorldCoord);
 
-            // Add neighbors
-            result.add(new TownBlock(x - 1, z, position.getWorld()));
-            result.add(new TownBlock(x + 1, z, position.getWorld()));
-            result.add(new TownBlock(x, z - 1, position.getWorld()));
-            result.add(new TownBlock(x, z + 1, position.getWorld()));
+            for (WorldCoord wc : neighboringWorldCoords) {
+                TownBlock neighboringTownBlock = TownyAPI.getInstance().getTownBlock(wc);
+                if (neighboringTownBlock == null)
+                    continue;
 
+                Town neighboringTown = neighboringTownBlock.getTownOrNull();
+                if (neighboringTown == null)
+                    continue;
+
+                if (neighboringTown != townBlock.getTownOrNull())
+                    result.add(neighboringTownBlock);
+            }
         }
 
-
         return result;
+    }
+
+    private static List<WorldCoord> getNeighboringWorldCoords(WorldCoord wc) {
+        List<WorldCoord> worldCoordList = new ArrayList<>();
+        worldCoordList.add(wc.add(1, 0));
+        worldCoordList.add(wc.add(-1, 0));
+        worldCoordList.add(wc.add(0, 1));
+        worldCoordList.add(wc.add(0, -1));
+
+        return worldCoordList;
     }
 }
--- src/main/java/me/schismtism/townycompactnations/TownyCompactNations.java
+++ src/main/java/me/schismtism/townycompactnations/TownyCompactNations.java
@@ -1,6 +1,8 @@
 package me.schismtism.townycompactnations;
 
 import com.palmergames.bukkit.towny.event.NationPreAddTownEvent;
+import com.palmergames.bukkit.towny.object.Nation;
+import com.palmergames.bukkit.towny.object.Town;
 import com.palmergames.bukkit.towny.object.TownBlock;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -8,50 +10,37 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Collection;
 
-
 public final class TownyCompactNations extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        System.out.println("Plugin Started!");
+        getLogger().info("TownyCompactNations enabled");
         getServer().getPluginManager().registerEvents(this, this );
     }
 
-    //Fires when a towny town is invited to a nation
-
     @EventHandler
-    public void onJoinNation (NationPreAddTownEvent event) {
-        boolean neighboringNation = false;
+    public void onNationInviteTown(NationPreAddTownEvent event) {
+        Collection<TownBlock> neighborsWithTown = NeighborUtil.getNeighboringTownBlocksWithOtherTown(event.getTown().getTownBlocks());
+        boolean isTownBorderingNation = false;
 
-        // Sends TownBlocks of the invited town to NeighborUtil, which gets all the neighboring TownBlocks
+        for (TownBlock townBlock : neighborsWithTown) {
+            Town town = townBlock.getTownOrNull();
+            if (town == null)
+                continue;
 
-        Collection<TownBlock> originalPositions = event.getTown().getTownBlocks();
-        Collection<TownBlock> positionsAndNeighbors = NeighborUtil.getNeighbors(originalPositions);
+            Nation nation = town.getNationOrNull();
+            if (nation == null)
+                continue;
 
-        // Checks if the invited town has any chunks bordering it owned by a town in the inviting nation
-
-        for (TownBlock position : positionsAndNeighbors) {
-
-            // for some reason it can only detect the invited town if position.getTownOrNull()
-            // though, position.getWorldCoord().getTownOrNull() works fine
-
-            if (position.getWorldCoord().getTownOrNull() != null) {
-
-                if (position.getWorldCoord().getTownOrNull().getNationOrNull() != null && event.getNation() == position.getWorldCoord().getTownOrNull().getNationOrNull()) {
-
-                    neighboringNation = true;
-                }
+            if (event.getNation() == nation) {
+                isTownBorderingNation = true;
+                break;
             }
         }
 
-        if (!neighboringNation) {
+        if (!isTownBorderingNation) {
             event.setCancelled(true);
-            System.out.println("Cancelled nation invite");
-            event.setCancelMessage("The invited town must border another nation member to join your nation");
-        }
-        else
-        {
-            System.out.println("Nation invite allowed");
+            event.setCancelMessage("Invited town must border another town in your nation");
         }
     }
 }
